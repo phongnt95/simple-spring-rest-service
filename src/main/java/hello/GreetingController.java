0db3c154baaa3f9d21f10aa930d13acb2f9925c6
phongnt95 <phong.nguyen@big-labs.com>
2017-11-15 09:45:30 +0700

    fix bug

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,7 +11,7 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/")
     public String index(@RequestParam(value="name", defaultValue="World") String name) {
         return "Hello, Welcome!!!";
     }
